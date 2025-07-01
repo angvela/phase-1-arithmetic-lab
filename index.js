5aa634b11f0d2c3fa49cb7f1ba4a37bf52489590
angvela <angelamusamali05@gmali.com>
2025-07-01 08:47:09 +0300

    Refactor index.html and index.js for clarity and structure

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Multiply num1 and num2 so the result is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Calculate the remainder of num3 divided by num4 so the remainder is 4
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+// Find the highest number in a set so the value returned is 20
+const max = Math.max(1, 5, 10, 15, 20);
